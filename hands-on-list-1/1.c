bd8d2bcd19c9621abc7ef4fcef3398913b51776c
JILS PATEL <100358865+JILSPATEL@users.noreply.github.com>
2025-08-17 17:47:41 +0530

    Update 1.c

--- hands-on-list-1/1.c
+++ hands-on-list-1/1.c
@@ -11,35 +11,19 @@ create the following types of a files using (i) shell command (ii) system call
 #include <sys/stat.h>
 
 int main() {
-    // Create an original file to link to
+
     FILE *fp = fopen("original.txt", "w");
-    if (!fp) {
-        perror("Error creating original file");
-        return 1;
-    }
     fprintf(fp, "This is the original file.\n");
     fclose(fp);
 
-    // Create soft link
-    if (symlink("original.txt", "soft_link.txt") == -1) {
-        perror("Error creating soft link");
-    } else {
-        printf("Soft link created: soft_link.txt -> original.txt\n");
-    }
+    symlink("original.txt", "soft_link.txt");
+    printf("Soft link created: soft_link.txt -> original.txt\n");
 
-    // Create hard link
-    if (link("original.txt", "hard_link.txt") == -1) {
-        perror("Error creating hard link");
-    } else {
-        printf("Hard link created: hard_link.txt -> original.txt\n");
-    }
+    link("original.txt", "hard_link.txt");
+    printf("Hard link created: hard_link.txt -> original.txt\n");
 
-    // Create FIFO
-    if (mkfifo("myfifo", 0666) == -1) {
-        perror("Error creating FIFO");
-    } else {
-        printf("FIFO created: myfifo\n");
-    }
+    mkfifo("myfifo", 0666);
+    printf("FIFO created: myfifo\n");
 
     return 0;
 }
