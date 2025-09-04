440b8069194d83067538382f525184ce0893a0d8
JILS PATEL <100358865+JILSPATEL@users.noreply.github.com>
2025-09-04 21:39:12 +0530

    Update 10.c

--- hands-on-list-1/10.c
+++ hands-on-list-1/10.c
@@ -11,11 +11,7 @@ int main() {
     int fd;
 
     fd = open("test10.txt", O_RDWR | O_CREAT | O_TRUNC, 0644);
-    if (fd == -1) {
-        perror("open");
-        return 1;
-    }
-
+   
     write(fd, "ABCDEFGHIJ", 10);       
     lseek(fd, 10, SEEK_CUR);          
     write(fd, "1234567890", 10);      
